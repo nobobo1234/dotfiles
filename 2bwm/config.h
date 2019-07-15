442aaedd763f26c6990c66f665476a2791a39293
nobobo1234 <noahvanboven@gmail.com>
2019-07-15 21:57:11 +0200

    Reducing borders

--- 2bwm/config.h
+++ 2bwm/config.h
@@ -32,7 +32,7 @@ static const bool inverted_colors = true;
 /*0) Outer border size. If you put this negative it will be a square.
  *1) Full borderwidth    2) Magnet border size
  *3) Resize border size  */
-static const uint8_t borders[] = {3,5,5,4};
+static const uint8_t borders[] = {3,2,2,4};
 /* Windows that won't have a border.
  * It uses substring comparison with what is found in the WM_NAME
  * attribute of the window. You can test this using `xprop WM_NAME`
